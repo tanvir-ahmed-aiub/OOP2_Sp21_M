8b06c585c9586367a4a7ed96d1cad20825241f2f
taanveerahmeed <taanveerahmeed@gmail.com>
2021-02-03 11:54:57 +0600

    Access modifiers and constructors

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 56; //literals
+            const int credit = 148;
+            a = 45;
+
+            int n; //declaration
+            //Console.WriteLine("n is " + n);
+
+            Student s1 = new Student("Tanvir","79461");
+
+           // s1.Name = "Tanvir Ahmed";
+           // s1.Id = "647679";
+
+           // Console.WriteLine("Name : " + s1.Name);
+            s1.ShowInfo();
+            Console.WriteLine(s1.Cgpa);
+           
+  
+        }
+    }
+}
---
+++ AccessModifiers/AccessModifiers/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    public class Student
+    {
+        string name;
+        string id;
+        float cgpa;
+        readonly int credit;
+        //properties or accessors
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public string Id
+        {
+            set { id = value; }
+            get { return id; }
+        }
+        public float Cgpa
+        {
+            get { return this.cgpa; }
+        }
+      
+
+        public Student() {
+            Console.WriteLine("Student default");
+            credit = 145;
+        }
+        public Student(string name, string id)
+        {
+            Console.WriteLine("Student Created with 2 parameter");
+            this.name = name;
+            this.id = id;
+           
+        }
+
+        public void ShowInfo() {
+            
+            Console.WriteLine("Name is: " + name);
+            Console.WriteLine("Id is: " + id);
+            Console.WriteLine("Cgpa is: " + cgpa);
+
+        }
+
+    }
+}
